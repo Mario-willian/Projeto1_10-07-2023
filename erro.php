b6abc8a30cae3aca223b0d14299d0ad6e2b92a0b
Italo <italochaves440@gmail.com>
2023-07-23 13:02:54 -0300

    configuracao htaccess

---
+++ erro.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Erro</title>
+    
+    <link rel="shortcut icon" href="img/logo.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  </head>
+  <body>
+
+    <div class="site-branding-area">
+        <div class="container">
+            <div class="row"><center>
+                <div class="col-sm-12">
+                    <div class="logo">
+                       
+                        <h1><a href="index"><img src="img/lg.jpg" style="width: 30%;"></a></h1>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="logo">
+                    <h1><b> Opa!</b></h1><h2>Não conseguimos encontrar a página que você está procurando. 
+                    <h3>Código do erro: 404</h3>
+                    <h4> Aqui estão alguns links úteis:</h4>
+                    <a style="font-size: 18px;" href="index">Página Inicial</a>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="logo">
+                    <img src="img/404.gif" style="width: 100%;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+  </body>
+</html>
