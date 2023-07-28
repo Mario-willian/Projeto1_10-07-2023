50c5ea0ff395849f3c2c2e55604803a84eba11d3
Mário Willian <59664316+Mario-willian@users.noreply.github.com>
2023-07-27 21:44:05 -0300

    Insert Usuarios e Impresas

--- classes/inserts/empresa.php
+++ classes/inserts/empresa.php
@@ -7,13 +7,58 @@ include_once "../../complements/inicio_classe.php";
 $empresa_cnpj = $dados['empresa_cnpj'];
 $empresa_nome_loja = $dados['empresa_nome_loja'];
 $empresa_razao_social = $dados['empresa_razao_social'];
+$enviar_empresa = "";
+
+//Recebendo Checkbox - Usuarios que terão acesso a empresa
+$usuario_acessos = null;
+if(isset($dados['usuario'])){
+    $usuario_acessos = $dados['usuario'];
+}
 
 //Recebendo a data Atual
 $data_criacao = date('Y-m-d H:i:s');
 
-//Inserir Ferias
-$inserir_empresa = "insert into empresas(id, cnpj, nome_loja, razao_social, status, data_criacao) values (NULL, '".$empresa_cnpj."', '".$empresa_nome_loja."', '".$empresa_razao_social."', 'Ativo', '".$data_criacao."');";
-$enviar_empresa = mysqli_query($conn, $inserir_empresa);
+//Verificando se o usuário já existe
+$pesquisa_empresa_existente = "SELECT cnpj FROM empresas WHERE cnpj = '".$empresa_cnpj."';";
+$resultado_empresa_existente = mysqli_query($conn, $pesquisa_empresa_existente);
+$row_empresa_existente = mysqli_fetch_assoc($resultado_empresa_existente);
+
+if($row_empresa_existente !== NULL){
+
+    // Caso existe, enviará mensagem de erro
+    $retorna = ['status' => false, 'msg' => "Erro ao Cadastrar a Empresa! CNPJ Já Cadastrado"];
+
+    // Converter o array em objeto e retornar para o JavaScript
+    echo json_encode($retorna);
+    exit;
+
+}else{
+
+    //Caso nao exista, seguirá o cadastro.
+    //Inserir Empresa
+    $inserir_empresa = "insert into empresas(id, cnpj, nome_loja, razao_social, status, data_criacao) values (NULL, '".$empresa_cnpj."', '".$empresa_nome_loja."', '".$empresa_razao_social."', 'Ativo', '".$data_criacao."');";
+    $enviar_empresa = mysqli_query($conn, $inserir_empresa);
+
+    //Pesquisar Empresa recem criado
+    $pesquisa_empresa = "SELECT id FROM empresas WHERE CNPJ = '".$empresa_cnpj."' AND data_criacao = '".$data_criacao."' ORDER BY id DESC LIMIT 1;";
+    $resultado_empresa = mysqli_query($conn, $pesquisa_empresa);
+    $row_empresa = mysqli_fetch_assoc($resultado_empresa);
+
+    //Select pra puxar o empresa recem criado para inserir o acesso dele
+    $id_empresa_recem_criado = $row_empresa['id']; 
+
+    //Loop para inserir acessos do usuário
+    if($usuario_acessos !== null){
+        for($i=0; $i < count($usuario_acessos); $i++){
+            //Inserir Acesso
+            $inserir_acesso = "insert into acessos(id, data_criacao, empresas_id, usuarios_id) values (NULL, '".$data_criacao."', '".$id_empresa_recem_criado."', '".$usuario_acessos[$i]."');";
+            $enviar_acesso = mysqli_query($conn, $inserir_acesso);
+        }
+    }
+
+}
+
+    
 
 //Mensagem de Sucesso ou Falha na operação
 if($enviar_empresa == 1){
@@ -21,7 +66,7 @@ if($enviar_empresa == 1){
     $retorna = ['status' => true, 'msg' => "Empresa Cadastrado com Sucesso!"];
 }else{
     // Criar o array com status e a mensagem de erro
-    $retorna = ['status' => false, 'msg' => "Erro ao Cadastrar a Empresa!"];
+    $retorna = ['status' => false, 'msg' => "Erro ao Cadastrar a Empresa! Verifique os Dados Inseridos"];
 }
 
 // Converter o array em objeto e retornar para o JavaScript
--- classes/inserts/usuario.php
+++ classes/inserts/usuario.php
@@ -7,30 +7,56 @@ include_once "../../complements/inicio_classe.php";
 $usuario_nome = $dados['usuario_nome'];
 $usuario_email = $dados['usuario_email'];
 $usuario_senha = $dados['usuario_senha'];
+$enviar_usuario = "";
 
-//Recebendo os campos da Combobox
-$empresas = implode(',', $_POST['op']);
-
-//Inserir cada item em uma variavel
-//trabalhando com checkbox em array no php
+//Recebendo Checkbox - Empresas que o Usuário tem acesso
+$usuario_acessos = null;
+if(isset($dados['empresa'])){
+    $usuario_acessos = $dados['empresa'];
+}
 
 //Recebendo a data Atual
 $data_criacao = date('Y-m-d H:i:s');
 
-//Inserir Usuario
-$inserir_usuario = "insert into usuarios(id, nome_completo, email, senha, status, data_criacao) values (NULL, '".$nome_completo."', '".$usuario_email."', '".$usuario_senha."', 'Ativo', '".$data_criacao."');";
-$enviar_usuario = mysqli_query($conn, $inserir_usuario);
+//Verificando se o usuário já existe
+$pesquisa_usuario_existente = "SELECT id FROM usuarios WHERE email = '".$usuario_email."';";
+$resultado_usuario_existente = mysqli_query($conn, $pesquisa_usuario_existente);
+$row_usuario_existente = mysqli_fetch_assoc($resultado_usuario_existente);
 
-//Select pra puxar o usuario recem criado para inserir o acesso dele
-$id_usuario_recem_criado = ""; 
+if($row_usuario_existente !== NULL){
 
-//Fazer loop para inserir todos acesso do usuario
-//Inserir Acesso
-$inserir_acesso = "insert into acessos(id, data_criacao, empresas_id, usuarios_id) values (NULL, '".$data_criacao."', '".$usuario_acesso."', '".$id_usuario_recem_criado."');";
-$enviar_acesso = mysqli_query($conn, $inserir_acesso);
+    // Caso existe, enviará mensagem de erro
+    $retorna = ['status' => false, 'msg' => "Erro ao Cadastrar o Usuário! E-mail Já Cadastrado"];
 
-echo $inserir_acesso;
-exit;
+    // Converter o array em objeto e retornar para o JavaScript
+    echo json_encode($retorna);
+    exit;
+
+}else{
+
+    //Caso nao exista, seguirá o cadastro.
+    //Inserir Usuario
+    $inserir_usuario = "insert into usuarios(id, nome_completo, email, senha, status, data_criacao) values (NULL, '".$usuario_nome."', '".$usuario_email."', '".$usuario_senha."', 'Ativo', '".$data_criacao."');";
+    $enviar_usuario = mysqli_query($conn, $inserir_usuario);
+
+    //Pesquisar Funcionário recem criado
+    $pesquisa_usuario = "SELECT id FROM usuarios WHERE email = '".$usuario_email."' AND data_criacao = '".$data_criacao."' ORDER BY id DESC LIMIT 1;";
+    $resultado_usuario = mysqli_query($conn, $pesquisa_usuario);
+    $row_usuario = mysqli_fetch_assoc($resultado_usuario);
+
+    //Select pra puxar o usuario recem criado para inserir o acesso dele
+    $id_usuario_recem_criado = $row_usuario['id']; 
+
+    //Loop para inserir acessos do usuário
+    if($usuario_acessos !== null){
+        for($i=0; $i < count($usuario_acessos); $i++){
+            //Inserir Acesso
+            $inserir_acesso = "insert into acessos(id, data_criacao, empresas_id, usuarios_id) values (NULL, '".$data_criacao."', '".$usuario_acessos[$i]."', '".$id_usuario_recem_criado."');";
+            $enviar_acesso = mysqli_query($conn, $inserir_acesso);
+        }
+    }
+
+}
 
 //Mensagem de Sucesso ou Falha na operação
 if($enviar_usuario == 1){
@@ -38,7 +64,7 @@ if($enviar_usuario == 1){
     $retorna = ['status' => true, 'msg' => "Usuário Cadastrado com Sucesso!"];
 }else{
     // Criar o array com status e a mensagem de erro
-    $retorna = ['status' => false, 'msg' => "Erro ao Cadastrar o Usuário!"];
+    $retorna = ['status' => false, 'msg' => "Erro ao Cadastrar o Usuário! Verifique os Dados Inseridos"];
 }
 
 // Converter o array em objeto e retornar para o JavaScript
