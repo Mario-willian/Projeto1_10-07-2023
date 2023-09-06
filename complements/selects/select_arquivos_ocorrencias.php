7a9f686a6145449ed3cb1e895298e23263dd75f8
Mario <mario_willian@outlook.com>
2023-09-05 23:14:13 -0300

    correcao ocorrencia

--- complements/selects/select_arquivos_ocorrencias.php
+++ complements/selects/select_arquivos_ocorrencias.php
@@ -9,21 +9,10 @@ $i=1;
 
 //Loop para listar em opções as empresas que o usuário tem acesso
 while ($row_arquivo_ocorrencia = mysqli_fetch_assoc($resultado_arquivo_ocorrencia)){
-        
-        /*if($i == 1){
-                echo "<img style=' width: 600px; height:600px;' src='../upload/ocorrencia/".$row_arquivo_ocorrencia['arquivo']."'> ";
-                //echo "</div></div>";
-        }else if($i == 2){
-                //echo "<div class='row'><div class='col-md-8'>";
-                echo "<img style=' width: 600px; height:600px;' src='../upload/ocorrencia/".$row_arquivo_ocorrencia['arquivo']."'>";
-                $i=0;
-        }
-        else{
-        echo "<br><img style=' width: 600px; height:600px;' src='../upload/ocorrencia/".$row_arquivo_ocorrencia['arquivo']."'> ";
-        }
-        $i++;*/
 
-        echo "<u><a target='_blank' href='../upload/ocorrencia/".$row_arquivo_ocorrencia['arquivo']."'>Documento ".$i."</a></u><br>";
+        if(!empty($row_arquivo_ocorrencia['arquivo'])){
+                echo "<u><a target='_blank' href='../upload/ocorrencia/".$row_arquivo_ocorrencia['arquivo']."'>Documento ".$i."</a></u><br>";
+        }
         $i++;
 }
 
